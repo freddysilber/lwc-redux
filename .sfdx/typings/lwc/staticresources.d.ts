45ccd2b76faab33e01ad012d578583a019b1a968
Freddy Silber <freddy.silber@cloudcoach.com>
2020-07-15 11:35:47 -0600

    config

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,12 @@
+declare module "@salesforce/resourceUrl/lodash" {
+    var lodash: string;
+    export default lodash;
+}
+declare module "@salesforce/resourceUrl/redux" {
+    var redux: string;
+    export default redux;
+}
+declare module "@salesforce/resourceUrl/reduxThunk" {
+    var reduxThunk: string;
+    export default reduxThunk;
+}
